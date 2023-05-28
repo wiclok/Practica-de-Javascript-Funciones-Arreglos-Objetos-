d06aab3ed599e586dae3d0623df1db1188b1d0cf
cespedesBrian <cepedesaguilerabrian@gmail.com>
2023-05-28 13:13:41 -0300

    Se terminó el punto 1 de Ejercicios de Funciones

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// Ejercicios de Funciones
+
+// 1) Escribe una función llamada "esMayorEdad" que reciba una edad como parámetro y devuelva true si la edad es mayor o igual a 18, y false en caso contrario.
+
+// const esMayorEdad = (edad) => {
+//     if (edad >= 18) {
+//         return true;
+//     }
+//     if (edad < 18) {
+//         return false;
+//     }
+// }
+
+// let res1 = esMayorEdad(12);
+// let res2 = esMayorEdad(18)
+
+// document.write(res1 + "<br>");
+// document.write(res2 + "<br>")
+

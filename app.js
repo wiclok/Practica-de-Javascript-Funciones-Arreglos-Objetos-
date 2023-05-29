830473428c335ef1cea45e3a606495a616463289
cespedesBrian <cepedesaguilerabrian@gmail.com>
2023-05-29 09:54:18 -0300

    Se terminó el punto 3 de Ejercicio de Funciones

--- app.js
+++ app.js
@@ -24,11 +24,28 @@
 // 2) Crea una función llamada "calcularAreaRectangulo" que tome dos parámetros: base y altura. La función debe calcular y devolver el área del rectángulo utilizando la fórmula área = base *altura.
 
 
-const calcularAreaRectangulo = (base,altura) => {
-    let area = base*altura;
-    return area;
+// const calcularAreaRectangulo = (base,altura) => {
+//     let area = base*altura;
+//     return area;
+// }
+
+// let res = calcularAreaRectangulo(5,2);
+
+// document.write(res);
+
+
+
+
+
+// 3) Implementa una función llamada "esPalindromo" que reciba una cadena como parámetro y devuelva true si la cadena es un palíndromo (se lee igual de izquierda a derecha y de derecha a izquierda), y false en caso contrario. Por ejemplo, para la cadena "radar" la función debería devolver true.
+
+
+const esPalindromo = (cadena) =>{
+    let array = cadena.split("");
+    let reverse = array.reverse();
+
+    return cadena == reverse.join("") ? "Si es" : "No es";
 }
 
-let res = calcularAreaRectangulo(5,2);
 
-document.write(res);
+document.write(esPalindromo("radar"));
